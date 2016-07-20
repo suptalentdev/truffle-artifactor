7d9d9d00758458eb9841a640c176171ac6876bce
suptalentdev <suptalentdev@gmail.com>
2016-07-20 11:02:09 +0200

    remove undefined attemps variable completely

--- classtemplate.js
+++ classtemplate.js
@@ -113,8 +113,6 @@ var Web3 = require("web3");
                   return reject(new Error("Transaction " + tx + " wasn't processed in " + (timeout / 1000) + " seconds!"));
                 }
 
-                attempts += 1;
-
                 setTimeout(make_attempt, 1000);
               });
             };
@@ -156,7 +154,6 @@ var Web3 = require("web3");
 
     instance.allEvents = contract.allEvents;
     instance.address = contract.address;
-    instance.transactionHash = contract.transactionHash;
   };
 
   // Use inheritance to create a clone of this contract,
